d30ce1614b04ffdc5466195d4f5884a1754a1303
fenglixuelilili <1913849404@qq.com>
2020-08-04 17:49:17 +0800

    支持hover 与 click两种事件 加入小圆角 hover加上节流

--- src/xf-popover.vue
+++ src/xf-popover.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="xf-popover">
-        <div class="temcont-wapper" ref='temcont-wapper' >
-            <div class="temcont" v-if="visiable" :style="{width:width+'px'}">
+        <div class="temcont-wapper" ref='temcont-wapper' v-if="visiable">
+            <div class="temcont"  :style="{width:width+'px'}">
                 <slot name='temcont'></slot> 
             </div>
         </div>
-        <div class="actived" @click="checkvisiable" ref='actived'>
+        <div class="actived"  ref='actived'>
             <slot></slot>
         </div>
     </div>
@@ -16,11 +16,26 @@ export default {
         width:{
             type:Number,
             default:150
+        },
+        position:{
+            type:String,
+            default:'top',
+            validator(value){
+                return ['top','bottom','left','right'].indexOf(value) >= 0
+            }
+        },
+        event:{
+            type:String,
+            default:'click',
+            validator(value){
+                return ['click','hover'].indexOf(value) >= 0
+            }
         }
     },
     data(){
         return {
-            visiable:false
+            visiable:false,
+            timer:null
         }
     },
     methods: {
@@ -33,23 +48,35 @@ export default {
         },
         moveEle(){
             this.$nextTick(()=>{
+                if(!this.$refs['temcont-wapper']){return}
                 document.body.appendChild(this.$refs['temcont-wapper'])
                 const {top,left,width,height} = this.$refs['actived'].getBoundingClientRect()
                 let distance = (this.$refs['temcont-wapper'].scrollWidth - this.$refs['actived'].scrollWidth)/2
                 //  绝对定位是相对于整个页面（包括滚动的距离的相对位置）
                 this.$refs['temcont-wapper'].style.top = window.scrollY + top - 5 + 'px'
                 this.$refs['temcont-wapper'].style.left = window.scrollX + left - distance + 'px'
+                this.$refs['temcont-wapper'].style.opacity = 1
             })
+            
         },
         openPopover(e){
-            this.visiable = true
-            this.moveEle()
-            document.addEventListener('click',this.bindPopover)
+            
+            clearInterval(this.timer)
+            this.timer = setTimeout(() => {
+                this.visiable = true
+                this.moveEle()
+                document.addEventListener('click',this.bindPopover)
+            }, 100);
+            
         },
         removeEvent(){
+            if(this.$refs['temcont-wapper']){
+                this.$refs['temcont-wapper'].style.opacity = 0
+            }
             document.removeEventListener('click',this.bindPopover)
         },
         closePopover(){
+            clearInterval(this.timer)
             this.visiable = false
             this.removeEvent()
         },
@@ -62,6 +89,14 @@ export default {
             }
         }
     },
+    mounted () {
+        if(this.event === 'click'){
+            this.$refs['actived'].addEventListener('click',this.checkvisiable)
+        }else{
+            this.$refs['actived'].addEventListener('mouseover',this.openPopover)
+            this.$refs['actived'].addEventListener('mouseleave',this.closePopover)
+        }
+    },
     created () {
         // 不能再初始化的时候直接监听  因为有多个实例组件的话 就要监听多次
         // document.addEventListener('click',()=>{
@@ -71,9 +106,10 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
+    $width:7px;
+    $height:7px;
     .xf-popover{
         display: inline-block;
-        
         .actived{
             
         }
@@ -83,14 +119,33 @@ export default {
         top:0;
         left: 0;
         transform: translateY(-100%);
+        background-color: #fff;
+        opacity: 0;
+        transition: opacity 0.3s;
         .temcont{
            box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+           border: 1px solid #ebeef5;
            padding: 10px;
            border-radius: 5px;
-        //    width: 150px;
+           position: relative;
+            word-wrap:break-word;
+            &::after{
+              width: $width;
+              height: $height;
+              content: '';
+              position: absolute;
+              left: 0;
+              right: 0;
+              margin: 0 auto;
+              top: calc(100% - 3.5px);
+              transform: rotateZ(45deg);
+              filter: drop-shadow(0 2px 12px rgba(0,0,0,.03));
+              border-bottom: 1px solid   #ebeef5;
+              border-right: 1px solid   #ebeef5;
+              background-color: #fff;
+              border-radius: 1px;
+           }
         }
-        // box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-        // padding: 10px;
     }
 </style>
 
